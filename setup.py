bb7dc2ac78d99a656af20ad3ae611b1c468b448e
Kohei Yamato <kohei-yamato@m3.com>
2018-09-07 11:35:13 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from distutils.core import setup, Extension
 
 boost_module = Extension('sort',
                          sources=['src/sort/sort.cpp'],
-                         include_dirs=['/usr/local/include'],
-                         library_dirs=['/usr/local/lib'],
                          libraries=['boost_python37'],
-                         extra_compile_args=['-std=c++14', '-O2', '-Wall'])
+                         extra_compile_args=['-O2', '-Wall'])
 
 setup(name='sort',
       version='0.1.0',
